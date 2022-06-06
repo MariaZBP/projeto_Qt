1314e195da4d93209f072d55f8a8713b38992cfe
Maria <ifpb@ifpb.com>
2022-06-06 15:57:02 -0300

    primeira tela botão

---
+++ projeto_EE_2022_1/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_my_button_clicked()
+{
+    QString texto;
+    texto = ui->entrada_dados->text();
+    ui->my_label->setText(texto);
+
+}
