4323c0ec5a7a0c46e4a9ed6af27e70caece03312
Maria Patricio <106992986+MariaZBP@users.noreply.github.com>
2022-06-25 08:50:28 -0300

    Conexão ao Banco de Dados
    Conexão ao BD e vereificação de usuário e senha

--- projeto_EE_2022_1/mainwindow.cpp
+++ projeto_EE_2022_1/mainwindow.cpp
@@ -1,6 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
 #include <telaprincipal.h>
+#include <QtSql>
+#include <QDebug>
+#include <QFileInfo>
+#include <QMessageBox>
+
+//Declara como banco de dados global
+static QSqlDatabase bancoDados = QSqlDatabase::addDatabase("QSQLITE");
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,8 +16,24 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    ui->statusBar->showMessage("Projeto Final - Técnicas de Programação EE-2022.1"
-                               " // Aluna: Maria da Conceição Z. B. Patrício Mat.: 20212610013");
+    //Cria a conexão com o banco de dados
+    bancoDados.setDatabaseName("C:/Users/Ceiça/Documents/Projeto_Qt/projeto_EE_2022_1/Banco de Dados0.db");
+
+    //Verifica se o banco de dados foi conectado
+    if(bancoDados.open()){
+
+        ui->statusBar->showMessage("Banco de dados conectado com sucesso!");
+
+    }else{
+
+        ui->statusBar->showMessage("Erro ao conectar com o banco de dados!");
+
+    }
+
+    //Mensagem de aviso para o usuário
+    ui->txtNome->setPlaceholderText("Digite seu nome");
+    ui->txtSenha->setPlaceholderText("Digite sua senha");
+
 }
 
 MainWindow::~MainWindow()
@@ -20,8 +44,43 @@ MainWindow::~MainWindow()
 void MainWindow::on_btnLogin_clicked()
 {
 
-    TelaPrincipal abrirTelaPrincipal;
-    abrirTelaPrincipal.setModal(true);
-    abrirTelaPrincipal.exec();
+    QString nomeUsuario = ui->txtNome->text();
+    QString senhaUsuario = ui->txtSenha->text();
+
+    if(bancoDados.isOpen()){
+
+        QSqlQuery consultaNoBancoDeDados;
+
+        //exec -> vai me retornar true se conseguir fazer a pesquisa
+        if(consultaNoBancoDeDados.exec("SELECT * FROM Login WHERE nome = '"+nomeUsuario+"' and senha = '"+senhaUsuario+"'")){
+
+            int contaRegistrosEncontrados = 0;
+
+            while(consultaNoBancoDeDados.next()){
+
+                contaRegistrosEncontrados++;
+
+            }
+
+            if(contaRegistrosEncontrados == 1){
+
+                this->close();
+                TelaPrincipal abrirTelaPrincipal;
+                abrirTelaPrincipal.setModal(true);
+                abrirTelaPrincipal.exec();
+
+            }else{
+
+                QMessageBox::critical(this, "Atenção!", "Usuário inválido ou senha inválida!");
+
+            }
+
+        }
+
+    }else{
+
+        QMessageBox::critical(this, "Atenção!", "O Banco de Dados não está aberto!");
+        return;
+    }
 
 }
